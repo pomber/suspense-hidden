f4651e14d921de8706e424f7fd97f931e4315384
pomber <pombopombopombo@gmail.com>
2018-06-21 17:36:15 -0300

    Commit

---
+++ index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+let done = false;
+let promise = null;
+
+const waitFor = text => {
+  console.log("waitFor");
+  if (done) return text;
+  if (!promise) {
+    promise = new Promise(resolve =>
+      setTimeout(() => {
+        done = true;
+        resolve();
+      }, 5000)
+    );
+  }
+  throw promise;
+};
+
+const AsyncText = ({ text }) => <h1>{waitFor(text)}</h1>;
+
+const App = () => (
+  <div hidden={true}>
+    <React.Timeout ms={0}>
+      {didTimeout =>
+        didTimeout ? <h1>Loading...</h1> : <AsyncText text="foo" />
+      }
+    </React.Timeout>
+  </div>
+);
+
+ReactDOM.render(
+  <React.unstable_AsyncMode>
+    <App />
+  </React.unstable_AsyncMode>,
+  document.getElementById("root")
+);
